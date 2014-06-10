ffd863d689071020562f8cd19e4a6932793d959b
Marc S. Brooks <marc@mbrooks.info>
2014-06-10 03:03:01 -0700

    Enclosed Tidy-Table call in module group with defined build/teardown lifecycles
    Added HTML generation result test

--- test/plugin/init.js
+++ test/plugin/init.js
@@ -1,30 +1,37 @@
-test('Initialize Tidy-Table', function() {
-	$('#qunit-fixture')
-		.TidyTable({
-			enableCheckbox : true,
-			enableMenu     : true
-		},
-		{
-			columnTitles : ['Column A','Column B','Column C'],
-			columnValues : [
-				['Row 1A','Row 1B','Row 1C'],
-				['Row 2A','Row 2B','Row 2C'],
-				['Row 3A','Row 3B','Row 3C']
-			],
-			menuOptions : [
-				['- Action -', null],
-				['Callback 1', { callback : doSomething1 }],
-				['Callback 2', { callback : doSomething2 }]
-			]
-		});
+module('Tidy-Table', {
+	setup : function() {
+		$('#qunit-fixture')
+			.TidyTable({
+				enableCheckbox : true,
+				enableMenu     : true
+			},
+			{
+				columnTitles : ['Column A','Column B','Column C'],
+				columnValues : [
+					['Row 1A','Row 1B','Row 1C'],
+					['Row 2A','Row 2B','Row 2C'],
+					['Row 3A','Row 3B','Row 3C']
+				],
+				menuOptions : [
+					['- Action -', null],
+					['Callback 1', { callback : doSomething1 }],
+					['Callback 2', { callback : doSomething2 }]
+				]
+			});
 
-	function doSomething1(rows) {
-		alert('callback1(rows=' + rows.length + ')');
+		function doSomething1(rows) {
+			alert('callback1(rows=' + rows.length + ')');
+		}
+
+		function doSomething2(rows) {
+			alert('callback2(rows=' + rows.length + ')');
+		}
+	},
+	teardown : function() {
+		// do nothing - preserve element structure
 	}
+});
 
-	function doSomething2(rows) {
-		alert('callback2(rows=' + rows.length + ')');
-	}
-
-	ok($('#qunit-fixture').has('table.tidy_table'))
+test('Generate HTML', function() {
+	ok($('#qunit-fixture').find(table), 'Table elements created');
 });
