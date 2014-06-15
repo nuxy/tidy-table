e07a4a8e27ff86fd5629cd2ff8a5f84c16300f35
Marc S. Brooks <marc@mbrooks.info>
2014-06-15 14:48:30 -0700

    Expanded testing to include all columns

--- test/plugin/sort.js
+++ test/plugin/sort.js
@@ -7,6 +7,8 @@ test('Sort Column A', function() {
 
 	equal($(table).find('tr:nth-child(1) > td:nth-child(2)').text(), 'Row 4A', "1st column expected is 'Row 4A'");
 	equal($(table).find('tr:nth-child(2) > td:nth-child(2)').text(), 'Row 3A', "2nd column expected is 'Row 3A'");
+	equal($(table).find('tr:nth-child(3) > td:nth-child(2)').text(), 'Row 2A', "3rd column expected is 'Row 2A'");
+	equal($(table).find('tr:nth-child(4) > td:nth-child(2)').text(), 'Row 1A', "4th column expected is 'Row 1A'");
 
 	ok($(table).find(col).trigger('click'), "Click event 'ascending'");
 	
@@ -14,6 +16,8 @@ test('Sort Column A', function() {
 
 	equal($(table).find('tr:nth-child(1) > td:nth-child(2)').text(), 'Row 1A', "1st column expected is 'Row 1A'");
 	equal($(table).find('tr:nth-child(2) > td:nth-child(2)').text(), 'Row 2A', "2nd column expected is 'Row 2A'");
+	equal($(table).find('tr:nth-child(3) > td:nth-child(2)').text(), 'Row 3A', "3rd column expected is 'Row 3A'");
+	equal($(table).find('tr:nth-child(4) > td:nth-child(2)').text(), 'Row 4A', "4th column expected is 'Row 4A'");
 });
 
 test('Sort Column B', function() {
@@ -25,6 +29,8 @@ test('Sort Column B', function() {
 
 	equal($(table).find('tr:nth-child(1) > td:nth-child(3)').text(), 'Row 4B', "1st column expected is 'Row 4B'");
 	equal($(table).find('tr:nth-child(2) > td:nth-child(3)').text(), 'Row 3B', "2nd column expected is 'Row 3B'");
+	equal($(table).find('tr:nth-child(3) > td:nth-child(3)').text(), 'Row 2B', "3rd column expected is 'Row 2B'");
+	equal($(table).find('tr:nth-child(4) > td:nth-child(3)').text(), 'Row 1B', "4th column expected is 'Row 1B'");
 
 	ok($(table).find(col).trigger('click'), "Click event 'ascending'");
 
@@ -32,6 +38,8 @@ test('Sort Column B', function() {
 
 	equal($(table).find('tr:nth-child(1) > td:nth-child(3)').text(), 'Row 1B', "1st column expected is 'Row 1B'");
 	equal($(table).find('tr:nth-child(2) > td:nth-child(3)').text(), 'Row 2B', "2nd column expected is 'Row 2B'");
+	equal($(table).find('tr:nth-child(3) > td:nth-child(3)').text(), 'Row 3B', "3rd column expected is 'Row 3B'");
+	equal($(table).find('tr:nth-child(4) > td:nth-child(3)').text(), 'Row 4B', "4th column expected is 'Row 4B'");
 });
 
 test('Sort Column C', function() {
@@ -43,6 +51,8 @@ test('Sort Column C', function() {
 
 	equal($(table).find('tr:nth-child(1) > td:nth-child(4)').text(), 'Row 4C', "1st column expected is 'Row 4C'");
 	equal($(table).find('tr:nth-child(2) > td:nth-child(4)').text(), 'Row 3C', "2nd column expected is 'Row 3C'");
+	equal($(table).find('tr:nth-child(3) > td:nth-child(4)').text(), 'Row 2C', "3rd column expected is 'Row 2C'");
+	equal($(table).find('tr:nth-child(4) > td:nth-child(4)').text(), 'Row 1C', "4th column expected is 'Row 1C'");
 
 	ok($(table).find(col).trigger('click'), "Click event 'ascending'");
 
@@ -50,4 +60,6 @@ test('Sort Column C', function() {
 
 	equal($(table).find('tr:nth-child(1) > td:nth-child(4)').text(), 'Row 1C', "1st column expected is 'Row 1C'");
 	equal($(table).find('tr:nth-child(2) > td:nth-child(4)').text(), 'Row 2C', "2nd column expected is 'Row 2C'");
+	equal($(table).find('tr:nth-child(3) > td:nth-child(4)').text(), 'Row 3C', "3rd column expected is 'Row 3C'");
+	equal($(table).find('tr:nth-child(4) > td:nth-child(4)').text(), 'Row 4C', "4th column expected is 'Row 4C'");
 });
