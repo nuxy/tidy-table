8ca04ff52440dd0c5a9764f3f0e9bbebe3bdc3af
Marc S. Brooks <marc@mbrooks.info>
2024-07-04 18:15:23 -0700

    Updated copyright year

--- src/tidy-table.js
+++ src/tidy-table.js
@@ -3,7 +3,7 @@
  *  Create a HTML table that can be sorted, selected and
  *  post-processed.
  *
- *  Copyright 2012-2023, Marc S. Brooks (https://mbrooks.info)
+ *  Copyright 2012-2024, Marc S. Brooks (https://mbrooks.info)
  *  Licensed under the MIT license:
  *  http://www.opensource.org/licenses/mit-license.php
  */
